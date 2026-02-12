dc56f883bab18e4ee7108dc887dddb743b6d48fe
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-12 20:05:02 -0300

    feat: configure main.dart with firebase init and DI setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+import 'shared/di/injection.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
-  runApp(Container()); 
+  setupDependencies();
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Mobile Challenge',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.deepPurple,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Flutter Mobile Challenge'),
+        ),
+      ),
+    );
+  }
 }
