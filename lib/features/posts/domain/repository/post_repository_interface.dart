e89e10e924bb2c404731052b0096db3287788d11
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-12 20:34:52 -0300

    feat: add getUserById and save methods to repository interface

--- lib/features/posts/domain/repository/post_repository_interface.dart
+++ lib/features/posts/domain/repository/post_repository_interface.dart
@@ -1,9 +1,11 @@
-import '../entities/post_entity.dart';
 import '../../../../shared/typedefs.dart';
+import '../entities/post_entity.dart';
+import '../entities/user_entity.dart';
 
 abstract class PostRepositoryInterface {
   AsyncResultPosts<List<PostEntity>> getPostList();
   AsyncResultPosts<PostEntity> getPostById(int id);
-  //AsyncResultPosts<void> savePost(PostEntity post);
-  //AsyncResultPosts<List<PostEntity>> getSavedPosts();
+  AsyncResultPosts<UserEntity> getUserById(int id);
+  AsyncResultPosts<void> savePost(PostEntity post);
+  AsyncResultPosts<List<PostEntity>> getSavedPosts();
 }
