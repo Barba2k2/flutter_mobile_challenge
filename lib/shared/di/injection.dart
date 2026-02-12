7ea3334dd63c56e61d392c013807589b708111de
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-12 19:30:08 -0300

    refactor: switch DI to Dio datasource and use relative imports

--- lib/shared/di/injection.dart
+++ lib/shared/di/injection.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_mobile_challenge/features/posts/data/datasource/post_datasource_interface.dart';
-import 'package:flutter_mobile_challenge/features/posts/data/datasource/post_http_datasource.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
-import 'package:flutter_mobile_challenge/features/posts/domain/repository/post_repository_interface.dart';
-import 'package:flutter_mobile_challenge/features/posts/domain/use_cases/get_posts_UseCase.dart';
+
+import '../../features/posts/data/datasource/post_datasource_interface.dart';
+import '../../features/posts/data/datasource/post_dio_datasource.dart';
+
 final getIt = GetIt.instance;
 
 void setupDependencies() {
@@ -14,8 +14,7 @@ void setupDependencies() {
 
   // ESCOLHA AQUI: Comente/descomente para trocar
   getIt.registerLazySingleton<PostsDatasourceInterface>(
-    () => PostsHttpDatasource(getIt<http.Client>()), // Para HTTP
-    // () => PostsDioDatasource(getIt<Dio>()), // Para Dio
+    () => PostsDioDatasource(getIt<Dio>()), // Para Dio
   );
 
   // Registre repositório, use cases, etc., usando PostsDatasourceInterface
