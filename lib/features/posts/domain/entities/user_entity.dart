4dca33bca75ab8f709bf08c19195b9866438cfda
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-12 20:34:39 -0300

    feat: add user entity to posts domain

---
+++ lib/features/posts/domain/entities/user_entity.dart
@@ -0,0 +1,21 @@
+class UserEntity {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  final String address;
+  final String website;
+  final String company;
+
+  const UserEntity({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.address,
+    required this.website,
+    required this.company,
+  });
+}
