426de9b8076f354f1101dba791b55954024bfab8
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-12 22:00:34 -0300

    refactor: add typed methods to auth repository interface

--- lib/features/auth/domain/repository/auth_repository_interface.dart
+++ lib/features/auth/domain/repository/auth_repository_interface.dart
@@ -1,6 +1,9 @@
-abstract class AuthRepositoryInterface {
-  Future loginUser();
-  Future registerUser();
+import '../../../../shared/typedefs.dart';
+import '../entities/user_entity.dart';
 
-  /// Future <List <PostEntity>> getPostList();
+abstract class AuthRepositoryInterface {
+  AsyncResultAuth<UserEntity> loginWithEmail(String email, String password);
+  AsyncResultAuth<UserEntity> registerWithEmail(String email, String password);
+  AsyncResultAuth<UserEntity> signInWithGoogle();
+  AsyncResultAuth<void> signOut();
 }
